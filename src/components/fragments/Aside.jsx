e4033c3129ee87f4b94e90a06cbdfc5be921e428
rafi andrea <rafiandrealesmana@gmail.com>
2024-07-30 13:15:28 +0700

    modified:   src/components/fragments/Aside.js

--- src/components/fragments/Aside.jsx
+++ src/components/fragments/Aside.jsx
@@ -1,6 +1,6 @@
 export default function Aside() {
     return (
-        <div className="w-[90px] bg-gray-300 h-screen">
+        <div className="flex flex-col items-center p-6 w-[90px] bg-gray-300 h-screen">
             <h1>Aside</h1>
         </div>
     )
